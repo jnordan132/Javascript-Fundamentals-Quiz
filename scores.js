4b7a41b2d603e648cad83832ad717ec9797eb339
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-07-20 21:12:12 -0400

    rewrote most of js files and htmls

--- scores.js
+++ scores.js
@@ -1,14 +1,14 @@
-var highScore = document.querySelector("#highScore");
-var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");
+var highScoreBtn = document.getElementById("highScore");
+var clearBtn = document.getElementById("clear");
+var retakeBtn = document.getElementById("retake");
 
 // Event listener to clear scores 
-clear.addEventListener("click", function() {
+clearBtn.addEventListener("click", function() {
     localStorage.clear();
     location.reload();
 });
-// Event listener got go to index page
-goBack.addEventListener("click", function() {
+// Event listener to go to index page
+retakeBtn.addEventListener("click", function() {
     window.location.replace("index.html");
 });
 // Retreives local stroage 
@@ -19,6 +19,6 @@ if (allScores !== null) {
     for (var i = 0; i < allScores.length; i++) {
         var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " " + allScores[i].score;
-        highScore.appendChild(createLi);
+        highScoreBtn.appendChild(createLi);
     }
 }
--- script.js
+++ script.js
@@ -1,150 +1,159 @@
-// DOM elements, starting variables, and Q&A array
-const currentTime = document.getElementById("currentTime");
-const timer = document.getElementById("startTime");
-const questionsDiv = document.getElementById("questionsDiv");
-const wrapper = document.getElementById("content");
-const ulCreate = document.createElement("ul");
+// Starting variables, assigned HTMl elements, and Q&A array
 var score = 0;
-var questionIndex = 0;
-var secondsLeft = 60;
-var holdInterval = 0;
-var timeSub = 5;
-var multiChoiceQuestions = [{
-        question: 'What does "DOM" stand for?',
-        choices: ["A. Document Oriented Model", "B. Designated Object Model", "C. Document Object Model", "D. Nothing, it doesnt exist"],
+var questionsIndex = 0;
+var startTime = 60;
+var pauseInterval = 0;
+var timeSubtraction = 5;
+const timeLeft = document.getElementById("timeLeft");
+const timer = document.getElementById("timer");
+const questionsContainer = document.getElementById("questionsContainer");
+const wrapper = document.getElementById("content");
+const createUl = document.createElement("ul");
+const multiChoiceQuestions = [{
+        question: "What does 'DOM' stand for?",
+        multipleChoices: ["A. Document Oriented Model", "B. Designated Object Model", "C. Document Object Model", "D. Nothing, it doesnt exist"],
         answer: "C. Document Object Model"
     },
     {
-        question: 'What does the "NaN" property mean in Javascript?',
-        choices: ["A. Not a number", "B. Its an error", "C. Nothing assigned", "D. Number added next"],
+        question: "What does the 'NaN' property mean in Javascript?",
+        multipleChoices: ["A. Not a number", "B. Its an error", "C. Nothing assigned", "D. Number added next"],
         answer: "A. Not a number"
     },
     {
-        question: 'Which HTML element do we use to link a JavaScript file to an HTML file?',
-        choices: ["A. <body>", "B. <link>", "C. <html>", "D. <script>"],
+        question: "Which HTML element do we use to link a JavaScript file to an HTML file?",
+        multipleChoices: ["A. <body>", "B. <link>", "C. <html>", "D. <script>"],
         answer: "D. <script>"
     },
     {
-        question: 'Which method returns the length of a string?',
-        choices: ["A. size()", "B. index()", "C. length()", "D. None of the above"],
-        answer: "C. length()"
+        question: "Which method returns the length of a string?",
+        multipleChoices: ["A. .size", "B. .index", "C. .length", "D. None of the above"],
+        answer: "C. .length"
     },
     {
-        question: 'How do you create a function in Javascript?',
-        choices: ["A. function = myFunction()", "B. function myFunction()", "C. function { myfunction()", "D. myFunction()"],
+        question: "How do you create a function in Javascript?",
+        multipleChoices: ["A. function = myFunction()", "B. function myFunction()", "C. function { myfunction()", "D. myFunction()"],
         answer: "B. function myFunction()"
     },
     {
-        question: 'How does a "for" loop start?',
-        choices: ["A. for(i < 5; i++)", "B. for i = 5; i++", "C. for(i = 0; i < 5; i++)", "D. for i++; i > 5;"],
+        question: "How does a 'for' loop start?",
+        multipleChoices: ["A. for(i < 5; i++)", "B. for i = 5; i++", "C. for(i = 0; i < 5; i++)", "D. for i++; i > 5;"],
         answer: "C. for(i = 0; i < 5; i++)"
     },
     {
         question: "Where do you put the <script> element in an HTML file?",
-        choices: ["A. <body>", "B. <head>", "C. <footer>", "D. <section>"],
+        multipleChoices: ["A. <body>", "B. <head>", "C. <footer>", "D. <section>"],
         answer: "A. <body>"
     },
     {
-        question: 'How do you add a comment in a JavaScript?',
-        choices: ["A. 'this is a comment'", "B. <-- this is a comment -->", "C. // this is a comment", "D. /* this is a comment /*"],
+        question: "How do you add a comment in a JavaScript?",
+        multipleChoices: ["A. 'this is a comment'", "B. <-- this is a comment -->", "C. // this is a comment", "D. /* this is a comment /*"],
         answer: "C. // this is a comment"
     }
 ];
+
 // Event listener to start timer, and display questions
 timer.addEventListener("click", function() {
-    if (holdInterval === 0) {
-        holdInterval = setInterval(function() {
-            secondsLeft--;
-            currentTime.textContent = "Time: " + secondsLeft;
-            if (secondsLeft <= 0) {
-                clearInterval(holdInterval);
-                allDone();
-                currentTime.textContent = "Time's up!";
+    if (pauseInterval === 0) {
+        pauseInterval = setInterval(function() {
+            startTime--;
+            timeLeft.textContent = "Time: " + startTime;
+            if (startTime <= 0) {
+                clearInterval(pauseInterval);
+                finished();
+                timeLeft.textContent = "Time's up!";
             }
         }, 1000);
     }
-    display(questionIndex);
+    display(questionsIndex);
 });
+
 // Displays questions and answers
-function display(questionIndex) {
-    questionsDiv.innerHTML = "";
-    ulCreate.innerHTML = "";
+function display(questionsIndex) {
+    questionsContainer.innerHTML = "";
+    createUl.innerHTML = "";
     for (var i = 0; i < multiChoiceQuestions.length; i++) {
-        var userQuestion = multiChoiceQuestions[questionIndex].question;
-        var userChoices = multiChoiceQuestions[questionIndex].choices;
-        questionsDiv.textContent = userQuestion;
+        let userQuestions = multiChoiceQuestions[questionsIndex].question;
+        var userAnswers = multiChoiceQuestions[questionsIndex].multipleChoices;
+        questionsContainer.textContent = userQuestions;
     }
-    userChoices.forEach(function(newItem) {
-        const listItem = document.createElement("li");
-        listItem.textContent = newItem;
-        questionsDiv.appendChild(ulCreate);
-        ulCreate.appendChild(listItem);
+    userAnswers.forEach(function(nextQuestion) {
+        let listItem = document.createElement("li");
+        listItem.textContent = nextQuestion;
+        questionsContainer.appendChild(createUl);
+        createUl.appendChild(listItem);
         listItem.addEventListener("click", (compare));
     })
-}
+};
+
 // Event to compare choices with answer
 function compare(event) {
-    var element = event.target;
+    let element = event.target;
     if (element.matches("li")) {
         var createDiv = document.createElement("div");
         createDiv.id = "createDiv";
-        if (element.textContent == multiChoiceQuestions[questionIndex].answer) {
+        if (element.textContent == multiChoiceQuestions[questionsIndex].answer) {
             score++;
-            createDiv.textContent = "Correct! The answer was:  " + multiChoiceQuestions[questionIndex].answer;
+            createDiv.textContent = "Correct!   " + multiChoiceQuestions[questionsIndex].answer;
         } else {
-            secondsLeft = secondsLeft - timeSub;
-            createDiv.textContent = "Wrong! The correct answer is:  " + multiChoiceQuestions[questionIndex].answer;
+            startTime = startTime - timeSubtraction;
+            createDiv.textContent = "Wrong! The correct answer is:  " + multiChoiceQuestions[questionsIndex].answer;
         }
     }
     // Question Index determines which question user is on
-    questionIndex++;
-    if (questionIndex >= multiChoiceQuestions.length) {
-        allDone();
+    questionsIndex++;
+    if (questionsIndex >= multiChoiceQuestions.length) {
+        finished();
     } else {
-        display(questionIndex);
+        display(questionsIndex);
     }
-    questionsDiv.appendChild(createDiv);
-}
+    questionsContainer.appendChild(createDiv);
+};
+
 // All done will append last page
-function allDone() {
-    questionsDiv.innerHTML = "";
-    currentTime.innerHTML = "";
+function finished() {
+    questionsContainer.innerHTML = "";
+    timeLeft.innerHTML = "";
     const createH1 = document.createElement("h1");
     createH1.id = "createH1";
     createH1.textContent = "All Done!"
-    questionsDiv.appendChild(createH1);
+    questionsContainer.appendChild(createH1);
     const createP = document.createElement("p");
     createP.id = "createP";
-    questionsDiv.appendChild(createP);
+    questionsContainer.appendChild(createP);
     // Calculates time remaining and replaces with score
-    if (secondsLeft >= 0) {
-        var timeRemaining = secondsLeft;
+    if (startTime >= 0) {
+        var timeRemaining = startTime;
         const createP2 = document.createElement("p");
-        clearInterval(holdInterval);
-        createP.textContent = "Your final score is: " + timeRemaining;
-        questionsDiv.appendChild(createP2);
+        clearInterval(pauseInterval);
+        createP.textContent = "Your final score is: " + timeRemaining * 2;
+        questionsContainer.appendChild(createP2);
     }
-    // Label
-    const createLabel = document.createElement("label");
-    createLabel.id = "createLabel", createLabel.textContent = "Enter your initials: ";
-    questionsDiv.appendChild(createLabel);
-    // input
-    const createInput = document.createElement("input");
-    createInput.type = "text", createInput.id = "initials", createInput.textContent = "";
-    questionsDiv.appendChild(createInput);
-    // submit
-    const createSubmit = document.createElement("button");
-    createSubmit.type = "submit", createSubmit.id = "Submit", createSubmit.textContent = "Submit";
-    questionsDiv.appendChild(createSubmit);
+    const infoPrompt = document.createElement("label");
+    infoPrompt.id = "createLabel";
+    infoPrompt.textContent = "Enter your initials: ";
+    questionsContainer.appendChild(infoPrompt);
+    // User initials
+    const userInitials = document.createElement("input");
+    userInitials.type = "text";
+    userInitials.id = "initials";
+    userInitials.textContent = "";
+    questionsContainer.appendChild(userInitials);
+    // Submit score and initials
+    const saveInfo = document.createElement("button");
+    saveInfo.type = "submit";
+    saveInfo.id = "Submit";
+    saveInfo.textContent = "Submit";
+    questionsContainer.appendChild(saveInfo);
+
     // Event listener for initials/score for local storage
-    createSubmit.addEventListener("click", function() {
-        var initials = createInput.value;
+    saveInfo.addEventListener("click", function() {
+        var initials = userInitials.value;
         if (initials === "") {
             console.log("No value entered!");
         } else {
             var finalScore = {
                 initials: initials,
-                score: timeRemaining
+                score: timeRemaining * 2
             }
             console.log(finalScore);
             var allScores = localStorage.getItem("allScores");
@@ -158,5 +167,5 @@ function allDone() {
             localStorage.setItem("allScores", newScore);
             window.location.replace("scores.html");
         }
-    });
+    })
 };
